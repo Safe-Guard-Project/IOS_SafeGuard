227579c40d19fef64733cc9387605b6c062e570c
omardjo <djebbi.omar@esprit.tn>
2023-11-09 09:51:57 +0100

    Initial Commit

---
+++ IOS_SafeGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_SafeGuard
+//
+//  Created by Omar.Djebbi on 9/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_SafeGuard/IOS_SafeGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_SafeGuardApp.swift
+//  IOS_SafeGuard
+//
+//  Created by Omar.Djebbi on 9/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS_SafeGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

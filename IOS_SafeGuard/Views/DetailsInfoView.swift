8994e40b8d7843313e87ba2d7a58493e8168205b
ekbell <ekbell@MM03L01.local>
2023-11-29 01:42:16 +0100

    DetailsInfo done

---
+++ IOS_SafeGuard/Views/DetailsInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsInfoView.swift
+//  IOS_SafeGuard
+//
+//  Created by ekbell on 29/11/2023.
+//
+
+import SwiftUI
+
+struct DetailsInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsInfoView()
+}

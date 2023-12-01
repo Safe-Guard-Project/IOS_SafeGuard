8064195c6a16a6a2cbfe96b85a209974a5323de9
SaibiDhia <dhiaeddine.saibi@esprit.tn>
2023-12-01 14:42:42 +0100

    CatasropheRepoImpl v2

--- IOS_SafeGuard/Services/CatastropheRepositoryImpl.swift
+++ IOS_SafeGuard/Services/CatastropheRepositoryImpl.swift
@@ -10,23 +10,33 @@ import Combine
 import Foundation
 
 class CatastropheRepositoryImpl: CatastropheRepository {
-    
-    
-    private let apiService: APIService
-    private let webServiceProvider: WebServiceProvider
-    
-    init(apiService: APIService, webServiceProvider: WebServiceProvider) {
-        self.apiService = apiService
-        self.webServiceProvider = webServiceProvider
+    private let apiManager: APIService
+
+    init(apiManager: APIService = ApiManager.shared) {
+        self.apiManager = apiManager
     }
-    
+
     func getCatastrophes() -> AnyPublisher<[Catastrophe], Error> {
-        let endpoint = CatastropheEndpoint.getCatastrophe
-                let request = APIRequest(method: .get, endpoint: endpoint)
-                return apiService.performRequest(request)
-                    .map(\.data)
-                    .decode(type: [Catastrophe].self, decoder: webServiceProvider.jsonDecoder)
-                    .eraseToAnyPublisher()
-        <#code#>
+        return apiManager.getCatastrophe()
+            .map { catastrophes in
+                catastrophes?.map { apiCatastrophe in
+                    return Catastrophe(
+                        _id: apiCatastrophe._id,
+                        titre: apiCatastrophe.titre,
+                        type: apiCatastrophe.type,
+                        tsunami: apiCatastrophe.tsunami,
+                        description: apiCatastrophe.description,
+                        date: apiCatastrophe.date,
+                        radius: apiCatastrophe.radius,
+                        magnitude: apiCatastrophe.magnitude,
+                        latitudeDeCatastrophe: apiCatastrophe.latitudeDeCatastrophe,
+                        longitudeDeCatastrophe: apiCatastrophe.longitudeDeCatastrophe,
+                        createdAt: apiCatastrophe.createdAt,
+                        updatedAt: apiCatastrophe.updatedAt
+                    )
+                } ?? []
+            }
+            .eraseToAnyPublisher()
     }
 }
+

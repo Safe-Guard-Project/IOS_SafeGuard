af09b7db8ae33848051deca9a88b104edae7b4c9
abir <karoui.abir@esprit.tn>
2023-12-03 22:20:28 +0100

    affecter a chaque button son cours par type

--- IOS_SafeGuard/Models/Cours.swift
+++ IOS_SafeGuard/Models/Cours.swift
@@ -60,6 +60,7 @@ struct Cours: Identifiable, Codable {
     }
 }
 */
+/*
 import Foundation
 import SwiftUI
 import CoreData
@@ -98,3 +99,61 @@ struct Cours: Identifiable, Codable {
     // func encode(to encoder: Encoder) throws { ... }
 }
 
+*/
+import Foundation
+
+struct Cours: Codable, Identifiable {
+    let id: String
+    let type: CoursType
+    let image: String
+    let description: String
+    enum CoursType: String, Codable, CaseIterable {
+        case Introduction = "Introduction"
+        case CAUSE = "CAUSE"
+        case CONSEQUENCE = "CONSEQUENCE"
+        case SIGNE = "SIGNE"
+        case Agir = "Agir"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case type = "Type"
+        case image
+        case description
+
+    }
+
+    init(id: String = "",
+         type: CoursType,
+         image: String ,
+         description: String
+         ) {
+        self.id = id
+        self.type = type
+        self.image = image
+        self.description = description
+    }
+    
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            id = try container.decode(String.self, forKey: .id)
+            type = try container.decode(CoursType.self, forKey: .type)
+            image = try container.decode(String.self, forKey: .image)
+            description = try container.decode(String.self, forKey: .description)
+        }
+        
+    }
+
+    /*func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(Nom_Article, forKey: .Nom_Article)
+        try container.encode(Nom_Client, forKey: .Nom_Client)
+        try container.encode(address_mail_Client, forKey: .address_mail_Client)
+        try container.encode(numero_Client, forKey: .numero_Client)
+        try container.encode(ville, forKey: .ville)
+        try container.encode(address_Client, forKey: .address_Client)
+    }
+}*/
